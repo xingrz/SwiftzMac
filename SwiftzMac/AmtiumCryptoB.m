dec2a2f3b9103b8df95f54b7c1b57d1ed8daac7f
XiNGRZ <chenxingyu92@gmail.com>
2013-04-10 17:02:18 +0800

    实现 3849 包以解决部分学校的兼容问题 #15

--- SwiftzMac/AmtiumCryptoB.m
+++ SwiftzMac/AmtiumCryptoB.m
@@ -1,25 +1,14 @@
 //
-//  AmtiumCrypto.m
+//  AmtiumCryptoB.m
 //  SwiftzMac
 //
-//  Created by XiNGRZ on 13-3-6.
+//  Created by XiNGRZ on 13-4-7.
 //  Copyright (c) 2013年 XiNGRZ. All rights reserved.
 //
 
-#import "AmtiumCrypto.h"
+#import "AmtiumCryptoB.h"
 
-unsigned char const bs_row[] = {
-    0x00, 0x01,
-    0x02, 0x03,
-    0x04, 0x05,
-    0x06, 0x07,
-    0x08, 0x09,
-    0x0a, 0x0b,
-    0x0c, 0x0d,
-    0x0e, 0x0f
-};
-
-unsigned char const bs_col[] = {
+unsigned char const bs_row_b[] = {
     0x00, 0x01,
     0x08, 0x09,
     0x04, 0x05,
@@ -30,7 +19,18 @@ unsigned char const bs_col[] = {
     0x0e, 0x0f
 };
 
-@implementation AmtiumCrypto
+unsigned char const bs_col_b[] = {
+    0x00, 0x01,
+    0x04, 0x05,
+    0x02, 0x03,
+    0x06, 0x07,
+    0x08, 0x09,
+    0x0c, 0x0d,
+    0x0a, 0x0b,
+    0x0e, 0x0f
+};
+
+@implementation AmtiumCryptoB
 
 + (NSData *)encrypt:(NSData *)data
 {
@@ -39,25 +39,29 @@ unsigned char const bs_col[] = {
     [data getBytes:buffer length:length];
 
     for (unsigned int i = 0; i < length; i++) {
-        unsigned char bl, of, row, col;
+        unsigned char bl_row, bl_col;
+        unsigned int row, col;
 
-        if (buffer[i] % 2 == 0) {
-            bl = floorf(buffer[i] / 32);
-            of = buffer[i] % 32;
-            row = floorf(of / 4);
+        unsigned char c1 = floorf(buffer[i] / 16);
+        unsigned char c2 = buffer[i] % 16;
+
+        if (c1 < 8) {
+            bl_row = floorf(c1 / 2) * 4;
+            bl_col = (c1 % 2) * 4;
         } else {
-            bl = floorf((buffer[i] - 1) / 32);
-            of = (buffer[i] - 1) % 32;
-            row = floorf(of / 4) + 8;
+            bl_row = floorf((c1 - 8) / 2) * 4;
+            bl_col = (c1 % 2 + 2) * 4;
         }
 
-        if (of % 4 == 0) {
-            col = bl * 2;
+        if (c2 % 2 == 0) {
+            row = bl_row + floorf(c2 / 8) * 2;
+            col = bl_col + (c2 % 8) / 2;
         } else {
-            col = bl * 2 + 1;
+            row = bl_row + floorf(c2 / 8) * 2 + 1;
+            col = bl_col + ((c2 - 1) % 8) / 2;
         }
 
-        buffer[i] = bs_row[row] * 16 + bs_col[col];
+        buffer[i] = bs_row_b[row] * 16 + bs_col_b[col];
     }
 
     return [NSData dataWithBytes:buffer length:length];
@@ -72,14 +76,17 @@ unsigned char const bs_col[] = {
     for (unsigned int i = 0; i < length; i++) {
         unsigned char c1 = floorf(buffer[i] / 16);
         unsigned char c2 = buffer[i] % 16;
-        
+
         unsigned char row = [self searchRowForValue:c1];
         unsigned char col = [self searchColForValue:c2];
 
-        unsigned char of = (row < 8) ? (row * 4) : ((row - 8) * 4 + 1);
-        unsigned char bl = (col % 2 == 0) ? (col * 16) : ((col - 1) * 16 + 2);
+        unsigned char b1 = floorf(row / 4) * 2 + floorf(col / 4);
+        if (col >= 8) b1 += 6;
 
-        buffer[i] = bl + of;
+        unsigned char b2 = col % 4 * 2 + row % 2;
+        if (row % 4 >= 2) b2 += 8;
+
+        buffer[i] = b1 * 16 + b2;
     }
 
     return [NSData dataWithBytes:buffer length:length];
@@ -88,7 +95,7 @@ unsigned char const bs_col[] = {
 + (unsigned char)searchRowForValue:(unsigned char)ch
 {
     for (unsigned int i = 0; i < 16; i++) {
-        if (bs_row[i] == ch) return i;
+        if (bs_row_b[i] == ch) return i;
     }
 
     return 0;
@@ -97,7 +104,7 @@ unsigned char const bs_col[] = {
 + (unsigned char)searchColForValue:(unsigned char)ch
 {
     for (unsigned int i = 0; i < 16; i++) {
-        if (bs_col[i] == ch) return i;
+        if (bs_col_b[i] == ch) return i;
     }
 
     return 0;
